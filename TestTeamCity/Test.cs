7c1ecc2a728eb05d51f7d8b1bff25bc599df1e62
Fabio Bombiela <fabio.bombiela@endava.com>
2020-01-29 11:29:57 -0500

    first

---
+++ TestTeamCity/Test.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestTeamCity
+{
+    [TestFixture]
+    class Test
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "https://www.google.co";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
