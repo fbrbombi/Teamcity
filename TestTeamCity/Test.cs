ead8ca49e11c1e2a20fc369d8319f5c3f0092fa3
Fabio Bombiela <fabio.bombiela@endava.com>
2020-01-30 19:50:14 -0500

    parallel

--- TestTeamCity/Test.cs
+++ TestTeamCity/Test.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Threading;
 
 namespace TestTeamCity
 {
@@ -10,18 +11,40 @@ namespace TestTeamCity
     {
         IWebDriver driver;
 
+
         [SetUp]
         public void startBrowser()
         {
             driver = new ChromeDriver();
         }
 
+
         [Test]
         public void test()
         {
             driver.Url = "https://www.google.co";
-            System.Console.WriteLine("AJIACO");
-            System.Console.WriteLine(Environment.GetEnvironmentVariable("Ambiente"));
+            Console.WriteLine("AJIACO1");
+            Console.WriteLine(Environment.GetEnvironmentVariable("Ambiente"));
+            Thread.Sleep(60000);
+        }
+
+        [Test]
+        public void test2()
+        {
+            driver.Url = "https://www.google.co";
+            Console.WriteLine("AJIACO2");
+            Console.WriteLine(Environment.GetEnvironmentVariable("Ambiente"));
+            Thread.Sleep(60000);
+
+        }
+        [Test]
+        public void test3()
+        {
+            driver.Url = "https://www.google.co";
+            Console.WriteLine("AJIACO3");
+            Console.WriteLine(Environment.GetEnvironmentVariable("Ambiente"));
+            Thread.Sleep(60000);
+
         }
 
         [TearDown]
